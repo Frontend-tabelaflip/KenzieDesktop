54a46202abaa04fb38d7c1086c3943763ea4234a
Julian <julian.cazuni@gmail.com>
2023-04-10 12:03:58 -0300

    10-04

---
+++ demos/S3_intro_JS/S3_demo1/script.js
@@ -0,0 +1,36 @@
+// console.log('Julian')
+// console.log('Kenzie Academy')
+/*alert('Julian')*/
+
+/* const nome = 'Julian';
+let idade = 31;
+
+{
+    let idade = 30;
+    console.log(idade);
+}
+
+console.log(nome)
+console.log(idade)
+
+idade = 32;
+
+console.log(nome)
+console.log(idade)*/
+
+//STRING
+let texto1 = 'Teste';
+let texto2 = "Teste";
+let texto3 = `Texto`;
+console.log(typeof texto1);
+
+//NUMBER
+let numero1 = 0;
+let numero2 = -131351384;
+let numero3 = 0.146548;
+console.log(typeof numero1);
+
+//BOOLEAN
+let boleano1 = true;
+let boleano2 = false;
+console.log(typeof boleano1);
