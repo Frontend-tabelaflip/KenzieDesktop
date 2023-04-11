231d6053af385a8e8c72d5a5fb4717eaf9471d85
Julian <julian.cazuni@gmail.com>
2023-04-11 10:44:33 -0300

    s3-demo2

--- demos/S3_intro_JS/S3-demo2/manipulacao_dados.js
+++ demos/S3_intro_JS/S3-demo2/manipulacao_dados.js
@@ -5,22 +5,24 @@ let idade = 31;
 
 //Julian Cazuni tem 31 anos de idade
 {
-let nomeCompleto1 = nome + ' ' + sobrenome + ' tem ' + idade + ' anos de idade';
-let nomeCompleto2 = `${nome} ${sobrenome} tem ${idade} anos de idade`;
+    let nomeCompleto1 = nome + ' ' + sobrenome + ' tem ' + idade + ' anos de idade';
+    let nomeCompleto2 = `${nome} ${sobrenome} tem ${idade} anos de idade`;
 
-// console.log(nomeCompleto2);
+    // console.log(nomeCompleto2);
 
-let countChar1 = nomeCompleto1.length;
-let countChar2 = nomeCompleto2.length;
+    let countChar1 = nomeCompleto1.length;
+    let countChar2 = nomeCompleto2.length;
 
-// console.log(countChar1,countChar2);
+    // console.log(countChar1,countChar2);
 }
-{
-    let primeiraLetra = nome[0];
-    let segundaLetra = sobrenome[0];
-    
-    let abreviacao = primeiraLetra + segundaLetra;
-    
-    let ultimo_nome = nome[nome.length - 1];
-    let ultimo_sobrenome = sobrenome[sobrenome.length - 1];
-}
+
+let primeiraLetra = nome[0];
+let segundaLetra = sobrenome[0];
+
+let abreviacao = primeiraLetra + segundaLetra;
+
+let ultimo_nome = nome[nome.length - 1];
+let ultimo_sobrenome = sobrenome[sobrenome.length - 1];
+
+
+console.log(ultimo_nome, ultimo_sobrenome);
