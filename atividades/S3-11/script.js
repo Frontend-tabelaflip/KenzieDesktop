d7f4328dc1a04560a06b87b59ecb25e2b60651c2
Julian <julian.cazuni@gmail.com>
2023-04-13 10:19:59 -0300

    13-04

---
+++ atividades/S3-11/script.js
@@ -0,0 +1,36 @@
+// Exercício 1
+// Crie um programa que concatene dois nomes e retorne uma frase de saudação.
+// Entrada: nome1 = "João", nome2 = "Silva" Saída: "Olá João Silva!"
+// let nome1 = 'João';
+// let nome2 = 'Silva';
+// console.log(`Olá ${nome1} ${nome2}!`)
+
+// Exercício 2
+// Crie um programa que adicione uma string ao final de outra string e retorne a string resultante.
+// Entrada: string1 = "hello", string2 = "world" Saída: "helloworld"
+// let string1 = 'hello'
+// let string2 = 'world'
+// let saida = string1 + string2
+// console.log(saida)
+
+// Exercício 3
+// Crie um programa que receba uma string em formato de número decimal e retorne o número inteiro equivalente.
+// Entrada: "12.5" Saída: 12
+// let numero = 12.5;
+// let numeroInteiro = parseInt(numero);
+// console.log(numeroInteiro)
+
+// Exercício 4
+// Crie um programa que converta uma string em formato de número inteiro para um número inteiro.
+// Entrada: "12" Saída: 12
+// let numero = 12;
+// let numeroInteiro = parseInt(numero);
+// console.log(numeroInteiro)
+
+// Exercício 5
+// Crie um programa que converta duas strings em formato de número inteiro para um número inteiro, faça uma soma desses números e apresente o resultado
+// Entrada 1: "12" Entrada 2: "3" Saída: 15
+// let num1 = 12;
+// let num2 = 3;
+// let soma = num1 + num2;
+// console.log(soma);
---
+++ demos/S3_intro_JS/S3-demo4/script.js
@@ -0,0 +1,54 @@
+/* Operadores Lógicos 
+
+let nome = 'julian';
+let senha = '1234';
+
+if((nome == 'julian' && senha == '1234') || (nome == 'cazuni' && senha == '9876')){
+    console.log('Acesso liberado');
+}else{
+    console.log('Acesso bloqueado');
+}
+*/
+
+
+/* AND
+true  && true  = true
+true  && false = false
+false && true  = false
+false && false = false
+*/
+/* OR
+true  || true  = true
+false || true  = true
+true  || false = true
+false || false = false
+*/
+
+/* Operadores de Incremento e Decremento
+
+let soma1 = 0;
+soma1 = soma1 + 1; //1
+soma1 = soma1 + 2; //3
+soma1 = soma1 + 3; //6
+
+let soma2 = 0;
+soma2++;  //1
+soma2+=2; //3
+soma2+=3; //6
+
+let count = 0;
+count = count + 1;
+count++;
+count+=1;
+
+let count2 = 0;
+count2 = count2 - 1;
+count2--;
+count2-=1;
+
+let mult = 1;
+mult*=3; // =3
+
+let div = 1;
+div*=3; // =3
+*/
